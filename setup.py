3512394e0dbfcd998deb784bdd67a425c439b4c6
Inkvi <inkvi@yandex-team.ru>
2012-01-31 18:15:22 +0400

    Fix problem with templates folder

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='debmaker',
       version='0.1',
       description='Tool for generating debian files',
       packages=['debmaker'],
-      package_data = {'debmaker': ['debmaker/templates/*']},
+      package_data = {'debmaker': ['templates/git/*','templates/pypi/*']},
       scripts=['debmaker/debmake'],
 )
